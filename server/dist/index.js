30e4de0b57bdc32eaccdf2b6472f0aff3b1e0f80
Morgan Mastrangelo <morgan172cm@gmail.com>
2023-10-20 06:40:40 -0400

    second fix

--- server/dist/index.js
+++ server/dist/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'https://face-verification.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Methods', 'POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
